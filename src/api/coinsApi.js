c8228e204dc652c5b438d1e4774035c4611765de
Atsui04 <bushkog92@gmail.com>
2026-04-24 17:53:40 +0300

    feat: add CoinGecko API integration service

---
+++ src/api/coinsApi.js
@@ -0,0 +1,18 @@
+const BASE_URL = "https://api.coingecko.com/api/v3";
+
+export async function getCoins() {
+  const res = await fetch(
+    `${BASE_URL}/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1`,
+    {
+      method: "GET",
+      headers: {
+        accept: "application/json",
+        "x-cg-demo-api-key": import.meta.env.VITE_COINGECKO_API_KEY,
+      },
+    },
+  );
+
+  if (!res.ok) throw new Error("Failed to get coins");
+
+  return res.json();
+}
