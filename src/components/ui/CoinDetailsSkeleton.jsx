da886bde1a5eb0a3c260a0a7b8db98c2a8246d0d
Atsui04 <bushkog92@gmail.com>
2026-05-16 15:17:48 +0300

    style: refine CoinDetailsSkeleton structure and responsive heights

--- src/components/ui/CoinDetailsSkeleton.jsx
+++ src/components/ui/CoinDetailsSkeleton.jsx
@@ -11,18 +11,34 @@ const CoinDetailsSkeleton = () => {
           <Skeleton height="11rem" width="100%" />
         </div>
         <div className="coin-links">
-          <Skeleton height="22rem" width="100%" />
+          {[...Array(3)].map((_, index) => (
+            <Skeleton
+              key={index}
+              className="coin-links__item-skeleton"
+              height="4rem"
+              width="100%"
+            />
+          ))}
         </div>
       </div>
       <div className="coin-page__stats">
         <div className="coin-graph">
-          <Skeleton className="skeleton-graph" width="100%" />
+          <Skeleton className="coin-graph__skeleton" width="100%" />
         </div>
         <div className="coin-stats">
-          <Skeleton height="26rem" width="100%" />
+          {[...Array(4)].map((_, index) => (
+            <div
+              key={index}
+              className="coin-stats__item"
+              style={{ border: "none" }}
+            >
+              <Skeleton height="2rem" width="30%" />
+              <Skeleton height="2rem" width="40%" />
+            </div>
+          ))}
         </div>
         <div className="coin-description">
-          <Skeleton height="48rem" width="100%" />
+          <Skeleton className="coin-description__skeleton" width="100%" />
         </div>
       </div>
     </div>
