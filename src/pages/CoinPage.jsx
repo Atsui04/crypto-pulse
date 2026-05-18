2fdd1b37f8cfb63319a1b2c2532dbd396a260a90
Atsui04 <bushkog92@gmail.com>
2026-05-18 18:08:38 +0300

    feat: add cryptocurrency watchlist using Zustand and localStorage

--- src/pages/CoinPage.jsx
+++ src/pages/CoinPage.jsx
@@ -2,18 +2,25 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router";
 
 import { getCoin } from "../api/coinsApi";
+import { useFavoritesStore } from "../stores/useFavoritesStore";
 
 import Loader from "../components/ui/Loader";
 import ErrorMessage from "../components/ui/ErrorMessage";
 import Back from "../components/ui/Back";
 import CoinDetails from "../components/coin-page/CoinDetails";
+import Save from "../components/ui/Save";
 
 const CoinPage = () => {
   const [coin, setCoin] = useState(null);
   const [error, setError] = useState("");
   const [isLoading, setIsLoading] = useState(false);
 
+  const favorites = useFavoritesStore((state) => state.favorites);
+  const toggleFavorite = useFavoritesStore((state) => state.toggleFavorite);
+
   const { id } = useParams();
+
+  const isActive = favorites.includes(id);
 
   useEffect(
     function () {
@@ -37,7 +44,10 @@ const CoinPage = () => {
 
   return (
     <div className="container">
-      <Back />
+      <div className="coin-page__item">
+        <Back />
+        <Save isActive={isActive} onClick={() => toggleFavorite(id)} />
+      </div>
       {error ? (
         <ErrorMessage error={error} />
       ) : (
